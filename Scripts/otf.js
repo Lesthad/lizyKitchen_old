512884f8514b4489c4a08015088d88d8ed2dfbc1
HunabKu <mksofttx@outlook.com>
2017-10-16 22:44:39 -0500

    1. Adding implementation for the autocomplete 2. Commenting the rules for the max-width since VS was displaying a warning to do't have empty rules

--- Scripts/otf.js
+++ Scripts/otf.js
@@ -11,19 +11,32 @@
 
         $.ajax(options).done(function(data){
             var $target = $($form.attr("data-otf-target"));
-            $target.replaceWith(data);
+            var $newHtml = $(data);
+            $target.replaceWith($newHtml);
+            $newHtml.effect("highlight");
+
         });
 
         return true;
+    };
+
+    var submitAutoCompleteForm = function (event, ui) {
+        var $input = $(this);
+        $input.val(ui.item.label);
+
+        var $form = $input.parents("form:first");
+        $form.submit();
     };
 
     var createAutocomplete = function () {
         var $input = $(this);
 
         var options = {
-            source: $input.attr("data-otf-autocomplete")
+            source: $input.attr("data-otf-autocomplete"),
+            select: submitAutoCompleteForm
         };
 
+        //$input.attr("class") = "list-group-item";
         $input.autocomplete(options);
     };
 
